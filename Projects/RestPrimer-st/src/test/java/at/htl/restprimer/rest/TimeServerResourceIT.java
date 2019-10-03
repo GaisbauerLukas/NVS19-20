53e67460f685090f6426b19d9afa7a361fb8b8a8
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2019-10-03 08:39:58 +0200

    Added 1Homework
    Added the File from the last lesson and added the Tests(Homework) fort it.

---
+++ Projects/RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -0,0 +1,32 @@
+package at.htl.restprimer.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TimeServerResourceIT {
+    private Client client;
+    private WebTarget tut;
+
+    @BeforeEach
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/RestPrimer/api/time");
+    }
+    @Test
+    public void fetchTime(){
+        Response response = this.tut.request((MediaType.TEXT_PLAIN)).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity((String.class));
+        assertTrue(payload.startsWith("Time: "));
+    }
+}
