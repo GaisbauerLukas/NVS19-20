b7e82da920e962425bde8c70810dc366a59ab60f
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2019-12-08 13:00:53 +0100

    Addded Files don in School

---
+++ Projects/src/main/java/rest/InitBean.java
@@ -0,0 +1,13 @@
+package rest;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+@Singleton
+@Startup
+public class InitBean {
+    @PostConstruct
+    private void init(){
+        System.out.println("*** it works ***");
+    }
+}
